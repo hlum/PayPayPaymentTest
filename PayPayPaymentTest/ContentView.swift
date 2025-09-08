72e3c3d11b922fdba04324337f0310daf6d2ecd4
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-09-08 12:30:30 +0900

    Initial Commit

---
+++ PayPayPaymentTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PayPayPaymentTest
+//
+//  Created by cmStudent on 2025/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PayPayPaymentTest/PayPayPaymentTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPayPaymentTestApp.swift
+//  PayPayPaymentTest
+//
+//  Created by cmStudent on 2025/09/08.
+//
+
+import SwiftUI
+
+@main
+struct PayPayPaymentTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
